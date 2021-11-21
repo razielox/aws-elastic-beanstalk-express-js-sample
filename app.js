f243dd975d946ccadaab789e3804e43a24f668a3
razielox <razielox@gmail.com>
2021-11-20 20:53:22 -0500

    se agregan nuevos cambios

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Este es el primer cambio para integrar, este es el segundo cambio ya con pipeline implementando'));
+app.get('/', (req, res) => res.send('Hello World! Este es el primer cambio para integrar, este es el segundo cambio ya con pipeline implementando y este es el 3er commit'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
